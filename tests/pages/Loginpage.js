2c04ef421415a5abcf62ef6bc793f5b2534b25f4
Raka Basu <121304933+5Raka@users.noreply.github.com>
2026-04-10 20:18:37 +0530

    Update Loginpage.js

---
+++ tests/pages/Loginpage.js
@@ -0,0 +1,24 @@
+class LoginPage {
+  get email() { return $('~emailInput'); }
+  get password() { return $('~passwordInput'); }
+  get loginBtn() { return $('~loginButton'); }
+  get errorMsg() { return $('~errorMessage'); }
+
+  async enterEmail(value) {
+    await this.email.setValue(value);
+  }
+
+  async enterPassword(value) {
+    await this.password.setValue(value);
+  }
+
+  async tapLogin() {
+    await this.loginBtn.click();
+  }
+
+  async getErrorMessage() {
+    return await this.errorMsg.getText();
+  }
+}
+
+module.exports = new LoginPage();
